40f26d147bb0ff773a1ed16cf2e6c3b46ceedde4
mwongeraE <evansmwongera@gmail.com>
2016-02-09 12:44:40 +0300

    while

--- dragon-slayer-ca.js
+++ dragon-slayer-ca.js
@@ -2,3 +2,8 @@ var slaying = true;
 var youHit = Math.floor(Math.random()*2); //sets youHit to arandom number that's either 0 or 1
 var damageThisRound = Math.floor(Math.random()*5 + 1); //set a random number between 1 and 5
 var totalDamage = 0;
+
+//while loop
+while(slaying) {
+    if(youHit = true){
+        console.log ("You hit the dragon");

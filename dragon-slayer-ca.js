26e41ae0a25f6e565e58f9faf8d6f29dd4035ed9
mwongeraE <evansmwongera@gmail.com>
2016-02-09 12:43:54 +0300

    total-damage

---
+++ dragon-slayer-ca.js
@@ -0,0 +1,4 @@
+var slaying = true;
+var youHit = Math.floor(Math.random()*2); //sets youHit to arandom number that's either 0 or 1
+var damageThisRound = Math.floor(Math.random()*5 + 1); //set a random number between 1 and 5
+var totalDamage = 0;
